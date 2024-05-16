d037f13a7f2d5ced556a04bdc49abccab37427c2
daniel <itzdanielmichael@gmail.com>
2024-05-16 14:11:39 -0400

    update auth layout

--- src/Layouts/AuthLayout/AuthLayout.jsx
+++ src/Layouts/AuthLayout/AuthLayout.jsx
@@ -1,5 +1,35 @@
-export const AuthLayout = ({ children }) => (
+import './AuthLayout.css';
+import SignUpImage from '@/assets/images/sign-up-screen.svg';
+import Logo from '@/assets/images/logo.svg';
+
+export const AuthLayout = ({
+  children,
+  image = SignUpImage,
+  title = (
+    <>
+      Royalty collections,
+      <br />
+      simplified. It’s next level!
+    </>
+  ),
+  subtitle = 'Revenue based invoice collection to make royalty collection as easy as...',
+}) => (
   <div className="AuthLayout__container">
-    <div className="AuthLayout__form">{children}</div>
+    <div className="inner-column">
+      <div className="AuthLayout__content">
+        <div className="AuthLayout__left">
+          <img src={Logo} alt="Logo" className="AuthLayout__left__logo" />
+
+          <img
+            src={image}
+            alt="Auth image"
+            className="AuthLayout__left__image"
+          />
+          <h1 className="AuthLayout__left__title">{title}</h1>
+          <h2 className="AuthLayout__left__subtitle">{subtitle}</h2>
+        </div>
+        <div className="AuthLayout__children AuthLayout__right">{children}</div>
+      </div>
+    </div>
   </div>
 );
--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
+      assets: `${path.resolve(__dirname, './src/assets/')}`,
       '@': path.resolve(__dirname, './src/'),
       components: `${path.resolve(__dirname, './src/components/')}`,
       views: `${path.resolve(__dirname, './src/views/')}`,
